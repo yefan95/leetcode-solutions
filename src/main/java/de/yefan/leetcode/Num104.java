febc01824b600f0fccd9f8fe250d14a01b141f9d
yefan <yefan9512@gmail.com>
2018-06-28 17:25:10 +0800

    Commit Maximum Depth of Binary Tree Solution

---
+++ src/main/java/de/yefan/leetcode/Num104.java
@@ -0,0 +1,47 @@
+package de.yefan.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Maximum Depth of Binary Tree
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ */
+public class Num104 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            ++depth;
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return depth;
+    }
+
+
+}
