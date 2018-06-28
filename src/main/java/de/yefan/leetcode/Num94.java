f417546589c94d991f2a67ef051039c39c4b946a
yefan <yefan9512@gmail.com>
2018-06-28 17:24:26 +0800

    Commit Binary Tree Inorder Traversal Solution

---
+++ src/main/java/de/yefan/leetcode/Num94.java
@@ -0,0 +1,35 @@
+package de.yefan.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Binary Tree Inorder Traversal
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+ */
+public class Num94 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        inorder(root, list);
+        return list;
+    }
+
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            inorder(root.left, list);
+            list.add(root.val);
+            inorder(root.right, list);
+        }
+    }
+}
