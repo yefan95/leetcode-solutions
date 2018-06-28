e3d9888fcf14837f80dffae93a19de489631a012
yefan <yefan9512@gmail.com>
2018-06-28 17:23:46 +0800

    Commit Search Insert Position Solution

---
+++ src/main/java/de/yefan/leetcode/Num35.java
@@ -0,0 +1,25 @@
+package de.yefan.leetcode;
+
+/**
+ * Search Insert Position
+ * https://leetcode.com/problems/search-insert-position/description/
+ */
+public class Num35 {
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return target > nums[left] ? left + 1 : left;
+    }
+
+}
