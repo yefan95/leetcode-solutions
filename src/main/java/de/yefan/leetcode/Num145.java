feac184eb531dd5ef96202632310fc8b832a24ab
yefan <yefan9512@gmail.com>
2018-06-28 17:28:29 +0800

    Commit Binary Tree Postorder Traversal Solution

---
+++ src/main/java/de/yefan/leetcode/Num145.java
@@ -0,0 +1,35 @@
+package de.yefan.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Binary Tree Postorder Traversal
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ */
+public class Num145 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        postorder(root, list);
+        return list;
+    }
+
+    private void postorder(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            postorder(root.left, list);
+            postorder(root.right, list);
+            list.add(root.val);
+        }
+    }
+}
