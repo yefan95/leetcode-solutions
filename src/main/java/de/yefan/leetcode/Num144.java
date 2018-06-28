562f963d17915def21825f828c92c1ac4617dfc4
yefan <yefan9512@gmail.com>
2018-06-28 17:26:18 +0800

    Commit Binary Tree Preorder Traversal Solution

---
+++ src/main/java/de/yefan/leetcode/Num144.java
@@ -0,0 +1,37 @@
+package de.yefan.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Binary Tree Preorder Traversal
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ */
+public class Num144 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        preorder(root, list);
+        return list;
+    }
+
+    private void preorder(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            System.out.println(root.val);
+            list.add(root.val);
+            preorder(root.left, list);
+            preorder(root.right, list);
+        }
+    }
+
+}
