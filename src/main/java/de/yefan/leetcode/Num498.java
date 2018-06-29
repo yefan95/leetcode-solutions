d761884ad3288a818d075b0816e33d528f6c2332
yefan <yefan9512@gmail.com>
2018-06-29 15:55:40 +0800

    Commit Diagonal Traverse Solution

---
+++ src/main/java/de/yefan/leetcode/Num498.java
@@ -0,0 +1,43 @@
+package de.yefan.leetcode;
+
+/**
+ * Diagonal Traverse
+ * https://leetcode.com/problems/diagonal-traverse/description/
+ */
+public class Num498 {
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix.length == 0) {
+            return new int[0];
+        }
+
+        int m = matrix.length, n = matrix[0].length;
+        int r = 0, c = 0;
+        int[] nums = new int[m * n];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = matrix[r][c];
+            // moving up
+            if ((r + c) % 2 == 0) {
+                if (c == n - 1) {
+                    r++;
+                } else if (r == 0) {
+                    c++;
+                } else {
+                    r--;
+                    c++;
+                }
+            }
+            // moving down
+            else {
+                if (r == m - 1) {
+                    c++;
+                } else if (c == 0) {
+                    r++;
+                } else {
+                    r++;
+                    c--;
+                }
+            }
+        }
+        return nums;
+    }
+}
