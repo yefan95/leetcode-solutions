31faabb3cbbe84453276aa5de78bdd833bcd0fd4
yefan <yefan9512@gmail.com>
2018-06-29 15:16:55 +0800

    Commit Find Pivot Index Solution

---
+++ src/main/java/de/yefan/leetcode/Num724.java
@@ -0,0 +1,35 @@
+package de.yefan.leetcode;
+
+import java.lang.annotation.Native;
+
+/**
+ * Find Pivot Index
+ * https://leetcode.com/problems/find-pivot-index/description/
+ */
+public class Num724 {
+
+    public int pivotIndex(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        int sum = 0, leftSum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftSum == sum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Num724 num724 = new Num724();
+        int[] nums = {-1, -1, -1, -1, 0, 1};
+        int index = num724.pivotIndex(nums);
+        System.out.println("index= " + index);
+    }
+
+}
