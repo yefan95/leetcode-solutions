ed402ed5c5b6ec885b353a9c2d0d1676b91db0fa
yefan <yefan9512@gmail.com>
2018-06-30 16:17:51 +0800

    Commit Add Binary Solution

---
+++ src/main/java/de/yefan/leetcode/Num5.java
@@ -0,0 +1,12 @@
+package de.yefan.leetcode;
+
+/**
+ * Longest Palindromic Substring
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ */
+public class Num5 {
+    public String longestPalindrome(String s) {
+
+        return "";
+    }
+}
