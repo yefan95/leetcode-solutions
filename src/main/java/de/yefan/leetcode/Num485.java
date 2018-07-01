a992000cba49304d8967df2eb67f4687b6664ce2
yefan <yefan9512@gmail.com>
2018-07-01 15:39:10 +0800

    Commit Max Consecutive Ones Solution

---
+++ src/main/java/de/yefan/leetcode/Num485.java
@@ -0,0 +1,38 @@
+package de.yefan.leetcode;
+
+
+/**
+ * Max Consecutive Ones
+ * https://leetcode.com/problems/max-consecutive-ones/description/
+ */
+public class Num485 {
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if (nums == null || nums.length <= 0) {
+            return -1;
+        }
+
+        int max = 0;
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                if (max < count) {
+                    max = count;
+                }
+                count = 0;
+            }
+        }
+
+        if (max < count) {
+            max = count;
+        }
+
+        return max;
+    }
+
+
+}
