e4ef4c01f874301c89f0e2e6e88909f46ba2b1a2
yefan <yefan9512@gmail.com>
2018-07-02 16:02:14 +0800

    add O(n) time complex and O(1) space solution

--- src/main/java/de/yefan/leetcode/Num189.java
+++ src/main/java/de/yefan/leetcode/Num189.java
@@ -15,4 +15,21 @@ public class Num189 {
             nums[j] = a[j];
         }
     }
+
+    public void rotate1(int[] nums, int k) {
+        k = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
 }
