54b194d507a10db3f9f95924539911586b760f8f
yefan <yefan9512@gmail.com>
2018-07-04 13:27:41 +0800

    add new solution

--- src/main/java/de/yefan/leetcode/linkedlist/Num21.java
+++ src/main/java/de/yefan/leetcode/linkedlist/Num21.java
@@ -65,4 +65,27 @@ public class Num21 {
         return root;
     }
 
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                head.next = l1;
+                l1 = l1.next;
+            } else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        if (l1 != null) {
+            head.next = l1;
+        }
+        if (l2 != null) {
+            head.next = l2;
+        }
+
+        return dummy.next;
+    }
+
 }
