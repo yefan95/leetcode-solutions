d80c0f52220b7b479d6835ca25b157a3b8fb7a95
yefan <yefan9512@gmail.com>
2018-07-06 14:45:01 +0800

    Commit Intersection of Two Arrays Solution

---
+++ src/main/java/de/yefan/leetcode/array/Num349.java
@@ -0,0 +1,94 @@
+package de.yefan.leetcode.array;
+
+import java.util.*;
+
+/**
+ * Intersection of Two Arrays
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ */
+public class Num349 {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (!set.contains(nums1[i])) {
+                set.add(nums1[i]);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int j = 0; j < nums2.length; j++) {
+            if (set.contains(nums2[j])) {
+                list.add(nums2[j]);
+                set.remove(nums2[j]);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
+
+    public int[] intersection1(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                set.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+    }
+
+    public int[] intersection2(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+}
