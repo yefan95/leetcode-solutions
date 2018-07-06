16d63498009e4e20ad79ae0a04ad67e50eed0e3d
yefan <yefan9512@gmail.com>
2018-07-06 21:38:22 +0800

    add new solution

--- src/main/java/de/yefan/leetcode/array/Num1.java
+++ src/main/java/de/yefan/leetcode/array/Num1.java
@@ -1,5 +1,10 @@
 package de.yefan.leetcode.array;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Two Sum
  * https://leetcode.com/problems/two-sum/description/
@@ -23,6 +28,31 @@ public class Num1 {
         result[0] = m;
         result[1] = n;
         return result;
+    }
+
+    public int[] twoSum1(int[] nums, int target) {
+        if (nums == null || nums.length <= 0) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && (map.get(target - nums[i]) != i)) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Num1 num1 = new Num1();
+        int[] nums = {3, 2, 4};
+        int[] res = num1.twoSum1(nums, 6);
+        if (res != null) {
+            System.out.println(res[0] + " " + res[1]);
+        }
     }
 
 }
