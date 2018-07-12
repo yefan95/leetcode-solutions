6795d210ea55f0069e8e82792b2c8b3591d7022f
yefan <yefan9512@gmail.com>
2018-07-12 09:11:43 +0800

    add new solution

--- src/main/java/de/yefan/leetcode/array/Num169.java
+++ src/main/java/de/yefan/leetcode/array/Num169.java
@@ -3,6 +3,7 @@ package de.yefan.leetcode.array;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Majority Element
@@ -10,6 +11,12 @@ import java.util.Map;
  */
 public class Num169 {
 
+    /**
+     * HashMap
+     *
+     * @param nums
+     * @return
+     */
     public int majorityElement(int[] nums) {
         if (nums.length <= 0) {
             return 0;
@@ -37,6 +44,12 @@ public class Num169 {
         return -1;
     }
 
+    /**
+     * Sorting
+     *
+     * @param nums
+     * @return
+     */
     public int majorityElement1(int[] nums) {
         if (nums.length <= 0) {
             return 0;
@@ -67,6 +80,10 @@ public class Num169 {
         return -1;
     }
 
+    /**
+     * @param nums
+     * @return
+     */
     public int majorityElement2(int[] nums) {
         int major = nums[0], count = 1;
         for (int i = 1; i < nums.length; i++) {
@@ -80,5 +97,114 @@ public class Num169 {
             }
         }
         return major;
+    }
+
+    /**
+     * Sorting
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement3(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+
+    private int randRange(Random rand, int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private int countOccurences(int[] nums, int num) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Randomization
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement4(int[] nums) {
+        Random rand = new Random();
+
+        int majorityCount = nums.length / 2;
+
+        while (true) {
+            int candidate = nums[randRange(rand, 0, nums.length)];
+            if (countOccurences(nums, candidate) > majorityCount) {
+                return candidate;
+            }
+        }
+    }
+
+    private int countInRange(int[] nums, int num, int lo, int hi) {
+        int count = 0;
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] == num) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int majorityElementRec(int[] nums, int lo, int hi) {
+        // base case; the only element in an array of size 1 is the majority
+        // element.
+        if (lo == hi) {
+            return nums[lo];
+        }
+
+        // recurse on left and right halves of this slice.
+        int mid = (hi - lo) / 2 + lo;
+        int left = majorityElementRec(nums, lo, mid);
+        int right = majorityElementRec(nums, mid + 1, hi);
+
+        // if the two halves agree on the majority element, return it.
+        if (left == right) {
+            return left;
+        }
+
+        // otherwise, count each element and return the "winner".
+        int leftCount = countInRange(nums, left, lo, hi);
+        int rightCount = countInRange(nums, right, lo, hi);
+
+        return leftCount > rightCount ? left : right;
+    }
+
+    /**
+     * Divide and Conquer
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement5(int[] nums) {
+        return majorityElementRec(nums, 0, nums.length - 1);
+    }
+
+    /**
+     * Boyer-Moore Voting Algorithm
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement6(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
     }
 }
