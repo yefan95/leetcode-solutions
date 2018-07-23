05a967eb144f01df1f33c51d4928f66d98823717
yefan <yefan9512@gmail.com>
2018-07-23 18:25:44 +0800

    add new solution

--- src/main/java/de/yefan/leetcode/array/Num1.java
+++ src/main/java/de/yefan/leetcode/array/Num1.java
@@ -46,6 +46,18 @@ public class Num1 {
         return null;
     }
 
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
     public static void main(String[] args) {
         Num1 num1 = new Num1();
         int[] nums = {3, 2, 4};
