162b8f2222f6627ed8d302f84dd9b5ff6f866e54
yefan <yefan9512@gmail.com>
2018-07-24 21:37:34 +0800

    add new solution

--- src/main/java/de/yefan/leetcode/btree/Num687.java
+++ src/main/java/de/yefan/leetcode/btree/Num687.java
@@ -32,4 +32,29 @@ public class Num687 {
         }
         return res[0];
     }
+
+    int max = 0;
+
+    public int longestUnivaluePath1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        helper(root, root.val);
+        return max;
+    }
+
+    private int helper(TreeNode root, int val) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left, root.val);
+        int right = helper(root.right, root.val);
+
+        max = Math.max(max, left + right);
+        if (root.val == val) {
+            return Math.max(left, right) + 1;
+        }
+
+        return 0;
+    }
 }
