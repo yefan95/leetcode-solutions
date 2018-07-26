b0977af3840d773659bc930a54ee00afff93c74f
yefan <yefan9512@gmail.com>
2018-07-26 10:57:15 +0800

    Commit Word Break Solution

---
+++ src/main/java/de/yefan/leetcode/dynamicprogramming/Num139.java
@@ -0,0 +1,23 @@
+package de.yefan.leetcode.dynamicprogramming;
+
+import java.util.List;
+
+/**
+ * Word Break
+ * https://leetcode.com/problems/word-break/description/
+ */
+public class Num139 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
