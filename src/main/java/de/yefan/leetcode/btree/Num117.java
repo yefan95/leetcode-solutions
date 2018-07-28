94d4c6ed08685f67ad547dc0315dea5bcee056da
yefan <yefan9512@gmail.com>
2018-07-28 16:54:57 +0800

    Commit Populating Next Right Pointers in Each Node II Solution

---
+++ src/main/java/de/yefan/leetcode/btree/Num117.java
@@ -0,0 +1,38 @@
+package de.yefan.leetcode.btree;
+
+/**
+ * Populating Next Right Pointers in Each Node II
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/description/
+ */
+public class Num117 {
+
+    class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+
+        TreeLinkNode(int x) {
+            val = x;
+        }
+    }
+
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode dummyHead = new TreeLinkNode(0);
+        TreeLinkNode pre = dummyHead;
+        while (root != null) {
+            if (root.left != null) {
+                pre.next = root.left;
+                pre = pre.next;
+            }
+            if (root.right != null) {
+                pre.next = root.right;
+                pre = pre.next;
+            }
+            root = root.next;
+            if (root == null) {
+                pre = dummyHead;
+                root = dummyHead.next;
+                dummyHead.next = null;
+            }
+        }
+    }
+}
