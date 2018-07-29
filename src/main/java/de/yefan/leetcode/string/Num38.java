b507a4305e2b874c7c76e08e02dfcd58fe08e047
yefan <yefan9512@gmail.com>
2018-07-29 23:29:14 +0800

    Commit Count and Say Solution

---
+++ src/main/java/de/yefan/leetcode/string/Num38.java
@@ -0,0 +1,36 @@
+package de.yefan.leetcode.string;
+
+/**
+ * Count and Say
+ * https://leetcode.com/problems/count-and-say/description/
+ */
+public class Num38 {
+
+    public String countAndSay(int n) {
+        String s = "1";
+        for (int i = 1; i < n; i++) {
+            s = countIdx(s);
+        }
+        return s;
+    }
+
+    public String countIdx(String s) {
+        StringBuilder sb = new StringBuilder();
+        char c = s.charAt(0);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                count++;
+            } else {
+                sb.append(count);
+                sb.append(c);
+                c = s.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(c);
+        return sb.toString();
+    }
+
+}
