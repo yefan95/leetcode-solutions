43fe3178541111b4a76c519abba54725e124ad9b
yefan <yefan9512@gmail.com>
2018-08-17 16:30:05 +0800

    Commit Department Top Three Salaries Solution

---
+++ src/main/java/de/yefan/leetcode/database/Num185.sql
@@ -0,0 +1,41 @@
+Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, DepartmentId int);
+Create table If Not Exists Department (Id int, Name varchar(255));
+Truncate table Employee;
+insert into Employee (Id, Name, Salary, DepartmentId) values ('1', 'Joe', '70000', '1');
+insert into Employee (Id, Name, Salary, DepartmentId) values ('2', 'Henry', '80000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId) values ('3', 'Sam', '60000', '2');
+insert into Employee (Id, Name, Salary, DepartmentId) values ('4', 'Max', '90000', '1');
+Truncate table Department;
+insert into Department (Id, Name) values ('1', 'IT');
+insert into Department (Id, Name) values ('2', 'Sales');
+
+
+-- Using JOIN and sub-query
+
+SELECT e1.Name AS 'Employee', e1.Salary
+FROM Employee AS e1
+WHERE 3 >
+(
+  SELECT count(DISTINCT e2.Salary)
+  FROM Employee AS e2
+  WHERE e2.Salary > e1.Salary
+)
+;
+
+
+SELECT
+    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+FROM
+    Employee e1
+        JOIN
+    Department d ON e1.DepartmentId = d.Id
+WHERE
+    3 > (SELECT
+            COUNT(DISTINCT e2.Salary)
+        FROM
+            Employee e2
+        WHERE
+            e2.Salary > e1.Salary
+                AND e1.DepartmentId = e2.DepartmentId
+        )
+;
