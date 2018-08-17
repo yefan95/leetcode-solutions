8b580f27d47937aaf8679013181e796441adff8b
yefan <yefan9512@gmail.com>
2018-08-17 16:19:01 +0800

    Commit Consecutive Numbers Solution

---
+++ src/main/java/de/yefan/leetcode/database/Num180.sql
@@ -0,0 +1,48 @@
+Create table If Not Exists Logs (Id int, Num int);
+Truncate table Logs;
+insert into Logs (Id, Num) values ('1', '1');
+insert into Logs (Id, Num) values ('2', '1');
+insert into Logs (Id, Num) values ('3', '1');
+insert into Logs (Id, Num) values ('4', '2');
+insert into Logs (Id, Num) values ('5', '1');
+insert into Logs (Id, Num) values ('6', '2');
+insert into Logs (Id, Num) values ('7', '2');
+
+-- Using DISTINCT and WHERE clause
+
+SELECT *
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
+
++------+------+-------+--------+-------+--------+
+| Id   | Num  | Id(2) | Num(2) | Id(3) | Num(3) |
++------+------+-------+--------+-------+--------+
+| 1    | 1    | 2     | 1      | 3     | 1      |
++------+------+-------+--------+-------+--------+
+
+SELECT DISTINCT
+    l1.Num AS ConsecutiveNums
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.Id = l2.Id - 1
+    AND l2.Id = l3.Id - 1
+    AND l1.Num = l2.Num
+    AND l2.Num = l3.Num
+;
+
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
