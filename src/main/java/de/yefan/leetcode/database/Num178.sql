55b691dbd0ca420b050c91e5b9c13429aca7c000
yefan <yefan9512@gmail.com>
2018-08-17 16:19:34 +0800

    Commit Rank Scores Solution

---
+++ src/main/java/de/yefan/leetcode/database/Num178.sql
@@ -0,0 +1,21 @@
+Create table If Not Exists Scores (Id int, Score DECIMAL(3,2));
+insert into Scores (Id, Score) values ('1', '3.5');
+insert into Scores (Id, Score) values ('2', '3.65');
+insert into Scores (Id, Score) values ('3', '4.0');
+insert into Scores (Id, Score) values ('4', '3.85');
+insert into Scores (Id, Score) values ('5', '4.0');
+insert into Scores (Id, Score) values ('6', '3.65');
+
+SELECT Score,
+(SELECT COUNT(DISTINCT Score) FROM Scores WHERE Score >= s.Score) AS Rank
+FROM Scores s
+ORDER BY Score DESC;
+
+SELECT
+  s.Score,
+  (SELECT count(1)
+   FROM (SELECT DISTINCT Score
+         FROM scores) t
+   WHERE s.score <= t.Score) AS Rank
+FROM scores s
+ORDER BY Score DESC
