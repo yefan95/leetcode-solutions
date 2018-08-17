00d34958573d7d4034dfb921d11fba10df9ae417
yefan <yefan9512@gmail.com>
2018-08-17 17:05:39 +0800

    Commit Rising Temperature Solution

---
+++ src/main/java/de/yefan/leetcode/database/Num197.sql
@@ -0,0 +1,19 @@
+Create table If Not Exists Weather (Id int, RecordDate date, Temperature int);
+insert into Weather (Id, RecordDate, Temperature) values ('1', '2015-01-01', '10');
+insert into Weather (Id, RecordDate, Temperature) values ('2', '2015-01-02', '25');
+insert into Weather (Id, RecordDate, Temperature) values ('3', '2015-01-03', '20');
+insert into Weather (Id, RecordDate, Temperature) values ('4', '2015-01-04', '30');
+
+SELECT a.Id
+FROM Weather a, Weather b
+WHERE DATEDIFF(a.RecordDate, b.RecordDate) = 1
+AND a.Temperature > b.Temperature;
+
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.RecordDate, w.RecordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
