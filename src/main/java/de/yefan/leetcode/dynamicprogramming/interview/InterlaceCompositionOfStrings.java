ab9716257df967c51f55049e4e0c626d322d4d8a
yefan <yefan9512@gmail.com>
2018-08-20 18:25:12 +0800

    优化空间

--- src/main/java/de/yefan/leetcode/dynamicprogramming/interview/InterlaceCompositionOfStrings.java
+++ src/main/java/de/yefan/leetcode/dynamicprogramming/interview/InterlaceCompositionOfStrings.java
@@ -9,11 +9,13 @@ public class InterlaceCompositionOfStrings {
 
     public static void main(String[] args) {
         String str1 = "AB", str2 = "12", aim = "1A2B";
-        boolean res = isCross(str1, str2, aim);
+        boolean res = isCross1(str1, str2, aim);
+        System.out.println(res);
+        res = isCross2(str1, str2, aim);
         System.out.println(res);
     }
 
-    public static boolean isCross(String str1, String str2, String aim) {
+    public static boolean isCross1(String str1, String str2, String aim) {
         if (str1 == null || str2 == null || aim == null) {
             return false;
         }
@@ -48,4 +50,46 @@ public class InterlaceCompositionOfStrings {
 
         return dp[ch1.length][ch2.length];
     }
+
+
+    public static boolean isCross2(String str1, String str2, String aim) {
+        if (str1 == null || str2 == null || aim == null) {
+            return false;
+        }
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+
+        char[] chaim = aim.toCharArray();
+
+        if (chaim.length != (ch1.length + ch2.length)) {
+            return false;
+        }
+
+        char[] longs = ch1.length >= ch2.length ? ch1 : ch2;
+        char[] shorts = ch1.length < ch2.length ? ch1 : ch2;
+
+        boolean[] dp = new boolean[shorts.length + 1];
+
+        dp[0] = true;
+        for (int i = 1; i <= shorts.length; i++) {
+            if (shorts[i - 1] != chaim[i - 1]) {
+                break;
+            }
+            dp[i] = true;
+        }
+
+        for (int i = 1; i <= longs.length; i++) {
+            dp[0] = dp[0] && longs[i - 1] == chaim[i - 1];
+            for (int j = 1; j <= shorts.length; j++) {
+                if ((longs[i - 1] == chaim[i + j - 1] && dp[j]) || (shorts[j - 1] == chaim[i + j - 1] && dp[j - 1])) {
+                    dp[j] = true;
+                } else {
+                    dp[j] = false;
+                }
+            }
+        }
+
+        return dp[shorts.length];
+    }
+
 }
