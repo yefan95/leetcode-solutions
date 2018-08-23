45daf1e9d4ab4928a7cf5a4bb66f5e1b56a0ef05
yefan <yefan9512@gmail.com>
2018-08-23 19:05:58 +0800

    Commit Interleaving String Solution

---
+++ src/main/java/de/yefan/leetcode/dynamicprogramming/Num97.java
@@ -0,0 +1,42 @@
+package de.yefan.leetcode.dynamicprogramming;
+
+/**
+ * Interleaving String
+ * <p>
+ * https://leetcode.com/problems/interleaving-string/description/
+ */
+public class Num97 {
+
+    public static void main(String[] args) {
+        String s1 = "aabcc";
+        String s2 = "dbbca";
+        String s3 = "aadbbcbcac";
+        boolean res = isInterleave(s1, s2, s3);
+        System.out.println(res);
+    }
+
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) {
+            return false;
+        }
+        int len1 = s1.length(), len2 = s2.length();
+        boolean[][] dp = new boolean[len1 + 1][len2 + 1];
+
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = true;
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1))
+                            || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+
+        return dp[len1][len2];
+    }
+}
